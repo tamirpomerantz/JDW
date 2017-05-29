96fb30573b721ad922a7df3f39d8acece066ce66
tamir pomerantz <tamir@bnop.co>
2017-05-29 08:46:41 +0300

    fixing mobile direction

--- js/app.js
+++ js/app.js
@@ -528,16 +528,18 @@ function onWindowResize() {
         }
 
 
-        camera.aspect = window.innerWidth / window.innerHeight;
+     
+
+
+        
+    } else {
+        // take care of mobile resize event
+        
+    }
+       camera.aspect = window.innerWidth / window.innerHeight;
         camera.updateProjectionMatrix();
-
-    }
-
-
     renderer.setSize(window.innerWidth, window.innerHeight);
-    if (isMobile) {
-
-    }
+    
 }
 
 function distanceVector(v1, v2) {
